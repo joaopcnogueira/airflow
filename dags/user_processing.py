43ab015f2996b4f37b58755ad013e4ac9ec9ece8
João Nogueira <joao.nogueira@datarisk.io>
2022-05-25 12:50:53 -0300

    Refactor user_processing.py

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,28 +1,31 @@
+import os
 import sys
-sys.path.append("/home/missy/airflow")
-
-from airflow.models import DAG
-from airflow.providers.sqlite.operators.sqlite import SqliteOperator
-from airflow.providers.http.sensors.http import HttpSensor
-from airflow.providers.http.operators.http import SimpleHttpOperator
-from airflow.operators.python import PythonOperator
-from airflow.operators.bash import BashOperator
-
-from include.functions.processing_user import _processing_user
-
 import json
 from datetime import datetime
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+
+# path configs
+HOME_DIR=os.path.expanduser('~')
+AIRFLOW_DIR=os.path.join(HOME_DIR, "airflow")
+sys.path.append(AIRFLOW_DIR)
+
+# local imports
+from include.functions.processing_user import _processing_user
 
 # args common to all the tasks within the DAG
 default_args = {
     'start_date': datetime(2020, 1, 1)
 }
 
-
 with DAG(dag_id='user_processing', schedule_interval='@daily',
         default_args=default_args,
         catchup=False,
-        template_searchpath="/home/missy/airflow/include") as dag:
+        template_searchpath=os.path.join(AIRFLOW_DIR, "include")) as dag:
     
     # Define the tasks/operators
     # task 1: creating table
